3fa762b1f2f8d8d6bd0ce6e7e7c22e1d1bec9a84
ummagumm-a <siniy.vyacheslav@gmail.com>
2021-09-15 14:14:23 +0300

    Save info about model in the file.

--- train.cpp
+++ train.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include <fstream>
 #include <torch/torch.h>
 
 #include "Net.h"
 #include "NetProcessing.h"
 
 using namespace std;
+
+void save_net_info(int64_t m, int64_t n)
+{
+    ofstream net_info("../net_info.txt");
+    if (net_info.is_open())
+        net_info << m << " " << n << endl;
+
+    net_info.close();
+}
 
 int main(int argc, char** argv)
 {
@@ -34,6 +44,7 @@ int main(int argc, char** argv)
     NetProcessing::train(net, lr, "../datasets/train.csv", n_epochs);
     cout << "Model is trained" << endl;
     torch::save(net, "../model.pt");
+    save_net_info(m, n);
     cout << "Model is saved." << endl;
 
     return 0;
